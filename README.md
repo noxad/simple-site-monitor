86aefb97706ff55ccb55861c545d3d9077ad8ce8
Brett Rogers <brettrogers@gmail.com>
2012-07-28 00:32:15 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-simple-site-monitor
-===================
+Simple Site Monitor
+========================
+
+Simple console application that makes web requests to the configured URLs, validating that they work correctly. Run on a schedule (e.g., Windows scheduled task) for a simple website monitoring solution. 
+
+Background
+---------------------
+I wanted to monitor the availability of various website that I developer and/or host. Searching the Internet yielded a lot of nice pay options and some limited functionality free options. I figured I'd create my own basic monitor and just run it as a scheduled task on my Windows desktop at home. Been running it for years now and it's worked perfectly, giving me the simple monitoring that I needed. 
+
+Features
+---------------------
+
+* **Website Monitor**
+	* Configure as many URLs to monitor as you like
+	* Checks for an HTTP status 200 response
+	* Also checks for the existence of expected text in the HTML returned in the response
+* **Email Notifications**
+	* Configure your SMTP credentials and send email alerts when a URL monitor fails
+	* Includes details on the failure, i.e., HTTP status code, if expected text wasn't found, etc.
+
+How to Use
+---------------------	
+
+1. Build the solution
+2. Take a copy of *SimpleSiteMonitor.exe* and *SimpleSiteMonitor.exe.config* from the build output and place them in a location from where you want the application to run
+3. Open *SimpleSiteMonitor.exe.config* in a text editor
+4. Configure the URLs to monitor:
+	- Create entries in the *appSettings* node:
+	```xml
+	<appSettings>
+		<!--add key="uniqueName" value="http://yourUrlHere.com,text to look for in HTML"/-->
+		<add key="google" value="http://www.google.com,I'm feeling lucky"/>
+	</appSettings>
+	```
+	- Set the SMTP values for sending email:
+	```xml
+	<applicationSettings>
+		<SimpleSiteMonitor.Settings>
+			<setting name="SmtpServer" serializeAs="String">
+				<value>smtp.gmail.com</value>
+			</setting>
+			<setting name="SmtpPort" serializeAs="String">
+				<value>587</value>
+			</setting>
+			<setting name="FromAddress" serializeAs="String">
+				<value>youraccount@gmail.com</value>
+			</setting>
+			<setting name="ToAddress" serializeAs="String">
+				<value>youraccount@gmail.com</value>
+			</setting>
+			<setting name="Password" serializeAs="String">
+				<value>yourPassword</value>
+			</setting>
+		</SimpleSiteMonitor.Settings>
+	</applicationSettings>
+	```
+5. Create a Windows scheduled task to execute *SimpleSiteMonitor.exe" on the schedule of your choosing
+6. Done
---
+++ SimpleSiteMonitor/Monitor.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+
+namespace SimpleSiteMonitor
+{
+    class Monitor
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.Specialized.NameValueCollection appSettings = System.Configuration.ConfigurationManager.AppSettings;
+            
+            foreach (string monitor in appSettings.AllKeys)
+            {
+                string[] urlAndText = appSettings[monitor].Split(',');
+                string url = urlAndText[0];
+                string textToLookFor = urlAndText[1];
+
+                CheckUrl(url, textToLookFor);
+            }
+        }
+
+        /// <summary>
+        /// Makes a web request using the specified URL and verifies whether or not the request succeeds
+        /// </summary>
+        /// <param name="url">URL to check</param>
+        private static void CheckUrl(string url, string textToLookFor)
+        {
+            HttpWebResponse response = null;
+            string statusCode;
+            string statusDescription;
+
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                    string html = reader.ReadToEnd();
+
+                    if (html.ToLower().Contains(textToLookFor.ToLower()))
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        SendEmail("WEBSITE DOWN (expected text not found): " + url, "URL: " + url + Environment.NewLine
+                            + "The expected text \"" + textToLookFor + "\" was not found");
+                    }                    
+                }
+
+                statusCode = ((int)response.StatusCode).ToString();
+                statusDescription = response.StatusDescription;
+            }
+            // WebException has an HttpWebResponse member which can provide an HTTP status code and description, so handle it differently
+            catch (WebException webException)
+            {
+                response = (HttpWebResponse)webException.Response;
+
+                if (response != null)
+                {
+                    statusCode = ((int)response.StatusCode).ToString();
+                    statusDescription = response.StatusDescription;
+                }
+                else
+                {
+                    statusCode = "-1";
+                    statusDescription = webException.Message;
+                }
+
+                SendEmail("WEBSITE DOWN: " + url, "URL: " + url + Environment.NewLine
+                    + "Status Code: " + statusCode + Environment.NewLine
+                    + "Status Description: " + statusDescription);
+            }
+            catch (Exception generalException)
+            {
+                if (response != null)
+                {
+                    statusCode = ((int)response.StatusCode).ToString();
+                    statusDescription = response.StatusDescription;
+                }
+                else
+                {
+                    statusCode = "-1";
+                    statusDescription = generalException.Message;
+                }
+
+                SendEmail("WEBSITE DOWN: " + url, "URL: " + url + Environment.NewLine
+                    + "Status Code: " + statusCode + Environment.NewLine
+                    + "Status Description: " + statusDescription);
+            }
+        }
+
+        private static void SendEmail(string subject, string body)
+        {
+            MailAddress fromAddress = new MailAddress(Settings.Default.FromAddress);
+            string fromPassword = Settings.Default.Password;
+            MailAddress toAddress = new MailAddress(Settings.Default.ToAddress);            
+
+            using (SmtpClient smtp = new SmtpClient())
+            {
+                smtp.Host = Settings.Default.SmtpServer;
+                smtp.Port = int.Parse(Settings.Default.SmtpPort);
+                smtp.EnableSsl = true;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(fromAddress.Address, fromPassword);
+
+                using (MailMessage message = new MailMessage(fromAddress, toAddress))
+                {
+                    message.Subject = subject;
+                    message.Body = body;
+
+                    smtp.Send(message);
+                }
+            }
+        }
+    }
+}
